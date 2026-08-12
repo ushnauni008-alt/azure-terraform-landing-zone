e2d9ea75e059de0fbf85e434ddd2b91cc386e9c2
ushnauni008-alt <ushnauni008@gmail.com>
2026-08-13 00:05:39 +0530

    terraform practice file

---
+++ practice/main.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-usha"
+  location = "eastus"
+}
---
+++ practice/provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  backend "azurerm" {
+    use_cli              = true
+    resource_group_name  = "tfstate-rg"
+    storage_account_name = "b18g94"
+    container_name       = "tfstateusha"
+    key                  = "practice.terraform.tfstate"
+
+  }
+  required_version = ">= 1.5.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "5.0.1"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
