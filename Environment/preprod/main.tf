cb216c06ce95a84d66cf04cf9a5d0b5318f2d299
ushnauni008-alt <ushnauni008@gmail.com>
2026-09-22 18:13:04 +0530

    Reorganize Terraform structure and add CI workflow

--- Environment/preprod/main.tf
+++ Environment/preprod/main.tf
@@ -1,21 +1,21 @@
 module "resource_group" {
-  source = "./modules/resource_group"
+  source = "../../modules/resource_group"
   rgs    = var.resource_groups
 }
 module "virtual_network" {
-  source     = "./modules/virtual_network"
+  source     = "../../modules/virtual_network"
   depends_on = [module.resource_group]
   vnets      = var.virtual_networks
 }
 
 module "subnet" {
-  source     = "./modules/subnet"
+  source     = "../../modules/subnet"
   depends_on = [module.virtual_network]
   snets      = var.subnets
 }
 
 module "network_security_group" {
-  source     = "./modules/network_security_group"
+  source     = "../../modules/network_security_group"
   depends_on = [module.resource_group, module.application_security_group]
   nsgs       = var.network_security_groups
 
@@ -49,19 +49,19 @@ module "network_security_group" {
 }
 
 module "application_security_group" {
-  source     = "./modules/application_security_group"
+  source     = "../../modules/application_security_group"
   depends_on = [module.resource_group]
   asgs       = var.application_security_groups
 }
 
 module "public_ip" {
-  source     = "./modules/public_ip"
+  source     = "../../modules/public_ip"
   depends_on = [module.resource_group]
   pips       = var.public_ips
 }
 
 module "network_interface" {
-  source     = "./modules/network_interface"
+  source     = "../../modules/network_interface"
   depends_on = [module.subnet, module.network_security_group, module.application_security_group, module.public_ip]
   nics = {
     for key, nic in var.network_interfaces :
